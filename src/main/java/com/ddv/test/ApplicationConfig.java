45aa84a5f270e5301035bb9857c985bf7ef2d17d
en129it <dimdevr@gmail.com>
2019-09-22 23:13:58 +0200

    Create ApplicationConfig.java

---
+++ src/main/java/com/ddv/test/ApplicationConfig.java
@@ -0,0 +1,19 @@
+
+    // LocaleChangeInterceptor : used to change the locale based on HTTP request attribute value
+    // SessionLocalResolver
+    
+    @Bean
+    public AcceptHeaderLocaleResolver getLocaleResolver() {
+        AcceptHeaderLocaleResolver rslt = new AcceptHeaderLocaleResolver();
+        rslt.setSupportedLocales(Arrays.asList(Locale.US, Locale.FRENCH));
+        rslt.setDefaultLocale(Locale.US);
+        return rslt;
+    }
+    
+    // Attention use a method name so that the bean name is "messageSource"
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource rslt = new ResourceBundleMessageSource();
+        rslt.setBasename("messages");
+        return rslt;
+    }
